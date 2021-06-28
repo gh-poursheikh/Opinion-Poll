ac48c786e4c8dd40f6fab612c34c0322113bd77d
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-06-28 19:24:15 +0430

    Implement Parcelable
    To allow a custom object (Person in this case) to be parsed
    to another component they need to implement the
    android.os.Parcelable interface.

--- app/src/main/java/com/example/opinionpoll/model/Person.java
+++ app/src/main/java/com/example/opinionpoll/model/Person.java
@@ -1,6 +1,9 @@
 package com.example.opinionpoll.model;
 
-public class Person {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Person implements Parcelable {
     private int personId;
     private String personType;
     private String username;
@@ -78,4 +81,52 @@ public class Person {
     public void setVoted(int voted) {
         this.voted = voted;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.personId);
+        dest.writeString(this.personType);
+        dest.writeString(this.username);
+        dest.writeString(this.password);
+        dest.writeString(this.firstName);
+        dest.writeString(this.lastName);
+        dest.writeInt(this.voted);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.personId = source.readInt();
+        this.personType = source.readString();
+        this.username = source.readString();
+        this.password = source.readString();
+        this.firstName = source.readString();
+        this.lastName = source.readString();
+        this.voted = source.readInt();
+    }
+
+    protected Person(Parcel in) {
+        this.personId = in.readInt();
+        this.personType = in.readString();
+        this.username = in.readString();
+        this.password = in.readString();
+        this.firstName = in.readString();
+        this.lastName = in.readString();
+        this.voted = in.readInt();
+    }
+
+    public static final Parcelable.Creator<Person> CREATOR = new Parcelable.Creator<Person>() {
+        @Override
+        public Person createFromParcel(Parcel source) {
+            return new Person(source);
+        }
+
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
 }
