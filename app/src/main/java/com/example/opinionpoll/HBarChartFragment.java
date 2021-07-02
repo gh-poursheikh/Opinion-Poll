8969d96b69249721223426a28fd331c09e9597be
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-02 20:29:24 +0430

    Add fragments logic
    Add Java code to each fragment to load data from server
    and plot or update the corresponding chart with it

--- app/src/main/java/com/example/opinionpoll/HBarChartFragment.java
+++ app/src/main/java/com/example/opinionpoll/HBarChartFragment.java
@@ -1,59 +1,164 @@
 package com.example.opinionpoll;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.opinionpoll.databinding.FragmentHBarChartBinding;
+import com.example.opinionpoll.model.Report;
+import com.example.opinionpoll.utils.MyWebService;
+import com.example.opinionpoll.utils.PageViewModel;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HBarChartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String TAG = HBarChartFragment.class.getSimpleName();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private PageViewModel pageViewModel;
+    private FragmentHBarChartBinding binding;
+
+    private HorizontalBarChart hBarChart;
+    private Button refreshChart;
+    private ArrayList<Report> reportList;
 
     public HBarChartFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HBarChartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HBarChartFragment newInstance(String param1, String param2) {
+    public static HBarChartFragment newInstance() {
         HBarChartFragment fragment = new HBarChartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+
+        reportList = new ArrayList<>();
+        pullReport();
+        pageViewModel.setReport(reportList);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentHBarChartBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_h_bar_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_h_bar_chart, container, false);
+
+        hBarChart = view.findViewById(R.id.h_bar_chart);
+        refreshChart = view.findViewById(R.id.refresh_button_h_bar);
+
+        refreshChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pullReport();
+            }
+        });
+
+        pageViewModel.getReport().observe(getViewLifecycleOwner(), new Observer<ArrayList<Report>>() {
+            @Override
+            public void onChanged(ArrayList<Report> reports) {
+                pullReport();
+            }
+        });
+
+        return view;
+    }
+
+    public void plotBarChart() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        final ArrayList<String> barLabels = new ArrayList<>();
+
+        for (int i = 0; i < reportList.size(); i++) {
+            BarEntry barEntry = new BarEntry(i, (float) (reportList.get(i).getProductAverage()));
+            barEntry.setData(reportList.get(i).getProductName());
+            barEntries.add(barEntry);
+            barLabels.add(reportList.get(i).getProductName());
+
+            hBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(barLabels));
+            hBarChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+                @Override
+                public void onValueSelected(Entry e, Highlight h) {
+                    Toast.makeText(HBarChartFragment.this.getContext(), e.getData().toString(), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected() {
+
+                }
+            });
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Product Name");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData barData = new BarData();
+        barData.addDataSet(barDataSet);
+
+        hBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(barLabels));
+        XAxis xAxis = hBarChart.getXAxis();
+        xAxis.setLabelRotationAngle(45f);
+
+        hBarChart.animateY(1000);
+        hBarChart.setData(barData);
+
+        Description description = new Description();
+        description.setText("");
+        hBarChart.setDescription(description);
+
+        hBarChart.invalidate();
+    }
+
+    public void pullReport() {
+        MyWebService webService = MyWebService.retrofit.create(MyWebService.class);
+        Call<ArrayList<Report>> call = webService.getReport();
+        call.enqueue(new Callback<ArrayList<Report>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Report>> call, Response<ArrayList<Report>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Network Error Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    reportList = response.body();
+                    plotBarChart();
+                    Log.d(TAG, "onResponse from requestReport(): " + response.toString());
+                } catch (Exception e) {
+                    Log.d(TAG, "onResponse from requestReport(): " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Report>> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/opinionpoll/PieChartFragment.java
+++ app/src/main/java/com/example/opinionpoll/PieChartFragment.java
@@ -1,64 +1,149 @@
 package com.example.opinionpoll;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PieChartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.opinionpoll.databinding.FragmentPieChartBinding;
+import com.example.opinionpoll.model.Report;
+import com.example.opinionpoll.utils.MyWebService;
+import com.example.opinionpoll.utils.PageViewModel;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class PieChartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = PieChartFragment.class.getSimpleName();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private PageViewModel pageViewModel;
+    private FragmentPieChartBinding binding;
+
+    private PieChart pieChart;
+    private Button refreshChart;
+    private ArrayList<Report> reportList;
 
     public PieChartFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PieChartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PieChartFragment newInstance(String param1, String param2) {
+    public static PieChartFragment newInstance() {
         PieChartFragment fragment = new PieChartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+
+        reportList = new ArrayList<>();
+        pullReport();
+        pageViewModel.setReport(reportList);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentPieChartBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pie_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_pie_chart, container, false);
+        pieChart = view.findViewById(R.id.pie_chart);
+        refreshChart = view.findViewById(R.id.refresh_button_pie);
+
+        refreshChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pullReport();
+            }
+        });
+
+        pageViewModel.getReport().observe(getViewLifecycleOwner(), new Observer<ArrayList<Report>>() {
+            @Override
+            public void onChanged(ArrayList<Report> reports) {
+                pullReport();
+            }
+        });
+
+        return view;
     }
+
+    public void plotPieChart() {
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < reportList.size(); i++) {
+            PieEntry pieEntry = new PieEntry((float) (reportList.get(i).getProductAverage()), reportList.get(i).getProductName());
+            pieEntry.setData(reportList.get(i).getProductName());
+            pieEntries.add(pieEntry);
+
+            pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+                @Override
+                public void onValueSelected(Entry e, Highlight h) {
+                    Toast.makeText(getContext(), e.getData().toString(), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected() {
+
+                }
+            });
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Product Name");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        PieData pieData = new PieData();
+        pieData.addDataSet(pieDataSet);
+
+        pieChart.animateXY(1000, 1000);
+        pieChart.setData(pieData);
+        pieChart.getDescription().setText("Products");
+        pieChart.invalidate();
+    }
+
+    public void pullReport() {
+        MyWebService webService = MyWebService.retrofit.create(MyWebService.class);
+        Call<ArrayList<Report>> call = webService.getReport();
+        call.enqueue(new Callback<ArrayList<Report>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Report>> call, Response<ArrayList<Report>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Network Error Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    reportList = response.body();
+                    plotPieChart();
+                    Log.d(TAG, "onResponse from requestReport(): " + response.toString());
+                } catch (Exception e) {
+                    Log.d(TAG, "onResponse from requestReport(): " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Report>> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+            }
+        });
+    }
+
 }
