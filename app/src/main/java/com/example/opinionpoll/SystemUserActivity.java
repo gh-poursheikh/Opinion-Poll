d0abede29b632fa3528840b0309a0d2ac4bf8743
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-03 22:06:50 +0430

    Check if external storage is both readable and writeable

--- app/src/main/java/com/example/opinionpoll/SystemUserActivity.java
+++ app/src/main/java/com/example/opinionpoll/SystemUserActivity.java
@@ -9,12 +9,16 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 
 import com.example.opinionpoll.utils.ViewPagerAdapter;
 import com.example.opinionpoll.databinding.ActivitySystemUserBinding;
 
 public class SystemUserActivity extends AppCompatActivity {
+
+    private static final String TAG = SystemUserActivity.class.getSimpleName();
 
     private ActivitySystemUserBinding binding;
 
@@ -39,5 +43,26 @@ public class SystemUserActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    // Check if external storage is available to at least read
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Log.d(TAG, "external storage Readable!");
+            return true;
+        }
+        return false;
+    }
+
+    // Check if external storage is available for read and write
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            Log.d(TAG, "external storage Writable!" );
+            return true;
+        }
+        return false;
     }
 }
