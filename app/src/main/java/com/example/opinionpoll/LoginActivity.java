51c0b56b307cd53394f361279001a0c694be708b
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-08 18:59:04 +0430

    Add ProgressBar to login activity

--- app/src/main/java/com/example/opinionpoll/LoginActivity.java
+++ app/src/main/java/com/example/opinionpoll/LoginActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameEditText;
     private EditText passwordEditText;
+    private ProgressBar progressBar;
     private Button loginButton;
     private String username;
     private String password;
@@ -34,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameEditText = findViewById(R.id.username_EditText);
         passwordEditText = findViewById(R.id.password_EditText);
+        progressBar = findViewById(R.id.progressBar);
         loginButton = findViewById(R.id.login_Button);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +50,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
+        progressBar.setVisibility(View.VISIBLE);
         MyWebService webService = MyWebService.retrofit.create(MyWebService.class);
         Call<Person> call = webService.login(username, password);
         call.enqueue(new Callback<Person>() {
@@ -54,6 +58,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onResponse(Call<Person> call, Response<Person> response) {
                 if (!response.isSuccessful()) {
                     Log.d(TAG, "onResponse: response.code(): " + response.code());
+                    progressBar.setVisibility(View.GONE);
                     Toast.makeText(LoginActivity.this, getString(R.string.network_error_code) + response.code(), Toast.LENGTH_LONG).show();
                     return;
                 }
@@ -63,6 +68,7 @@ public class LoginActivity extends AppCompatActivity {
                         if (person.getVoted() == 0) {
                             Intent intent = new Intent(getBaseContext(), NormalUserActivity.class);
                             intent.putExtra("EXTRA_SESSION_ID", person);
+                            progressBar.setVisibility(View.GONE);
                             startActivity(intent);
                             finish(); // When the users hit the back reportButton, they will not be able to return
                             // to the current activity because it has been killed off the Back Stack.)
@@ -70,11 +76,13 @@ public class LoginActivity extends AppCompatActivity {
                             usernameEditText.setText("");
                             passwordEditText.setText("");
                             usernameEditText.requestFocus();
+                            progressBar.setVisibility(View.GONE);
                             Toast.makeText(LoginActivity.this, getString(R.string.sorry_you_have_already_voted), Toast.LENGTH_SHORT).show();
                         }
                     } else {
                         Intent intent = new Intent(getBaseContext(), SystemUserActivity.class);
                         intent.putExtra("EXTRA_SESSION_ID", person);
+                        progressBar.setVisibility(View.GONE);
                         startActivity(intent);
                         finish(); // When the users hit the back reportButton, they will not be able to return
                         // to the current activity because it has been killed off the Back Stack.)
@@ -85,6 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                     usernameEditText.setText("");
                     passwordEditText.setText("");
                     usernameEditText.requestFocus();
+                    progressBar.setVisibility(View.GONE);
                     Toast.makeText(LoginActivity.this, getString(R.string.sorry_record_not_found), Toast.LENGTH_SHORT).show();
                 }
             }
@@ -92,6 +101,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<Person> call, Throwable t) {
                 Log.d(TAG, "onFailure: " + t.getMessage());
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
